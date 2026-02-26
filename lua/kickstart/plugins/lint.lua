e15b11cc281edb3b19575139157ca4d2a2490ad6
Alexis Ramírez <alexis.jrm.0395@gmail.com>
2026-02-26 19:58:07 -0400

    added linting

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,12 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        html = { 'htmlhint' },
+        typescript = { 'eslint_d' },
+        javascript = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
+        go = { 'golangcilint' },
+        php = { 'phpstan' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
